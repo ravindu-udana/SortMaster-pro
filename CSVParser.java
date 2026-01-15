a94a4ac6312d2edbac93a91467581b9e6ccdefbb
ravindu-udana <ravindurvudana@gmail.com>
2026-01-15 15:40:20 +0530

    get header

--- CSVParser.java
+++ CSVParser.java
@@ -1,4 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CSVParser {
+
+    public static List<String> getHeaders(File file) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = br.readLine();
+            if (line == null) return new ArrayList<>();
+            return parseLine(line);
+        }
+    }
 
 }
 
